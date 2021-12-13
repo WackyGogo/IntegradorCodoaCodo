21395a2d8c461a9bbd7a378d0c5a559562e79577
Javier Franco <ffranc@gmail.com>
2021-12-12 23:42:32 -0300

    Tabla encontrados y submits creados

--- js/crear.js
+++ js/crear.js
@@ -1,25 +1,56 @@
-function grabarPerdido(){
-    let a=document.getElementById("inputAnimalPerdido").value;
-    let z=document.getElementById("inputZonaPerdido").value;
-    let n=document.getElementById("inputNombrePerdido").value;
-    let c=document.getElementById("inputColorPerdido").value;
+function grabarPerdido() {
 
-    let perdidos={
-        nombre:n,
-        zona:z,
-        raza:a,
-        color:c,
+    let a = document.getElementById("inputAnimalPerdido").value;
+    let z = document.getElementById("inputZonaPerdido").value;
+    let n = document.getElementById("inputNombrePerdido").value;
+    let c = document.getElementById("inputColorPerdido").value;
+
+    let perdidos = {
+        nombre: n,
+        zona: z,
+        raza: a,
+        color: c,
     }
 
-    let url= "http://localhost:8080/perdidos";
+    let url = "http://localhost:8080/perdidos";
     var options = {
         method: 'POST',
         body: JSON.stringify(perdidos),
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         redirect: 'follow'
     }
 
-    fetch(url,options).then(function(){
+    fetch(url, options).then(function () {
         console.log("creado");
-    }).then(location.reload())   
+    }).then(location.reload())
+}
+
+function grabarEncontrado() {
+
+    document.get
+    if (REQUIRED) {
+        let a = document.getElementById("inputAnimalEncontrado").value;
+        let z = document.getElementById("inputZonaEncontrado").value;
+        let c = document.getElementById("inputColorEncontrado").value;
+        let s = document.getElementById("inputSexoEncontrado").value;
+
+        let encontrados = {
+            zona: z,
+            raza: a,
+            color: c,
+            sexo: s
+        }
+
+        let urlEnc = "http://localhost:8080/encontrados";
+        var options = {
+            method: 'POST',
+            body: JSON.stringify(encontrados),
+            headers: { 'Content-Type': 'application/json' },
+            redirect: 'follow'
+        }
+
+        fetch(urlEnc, options).then(function () {
+            console.log("creado");
+        }).then(location.reload())
+    }
 }
--- js/main.js
+++ js/main.js
@@ -2,18 +2,35 @@ const app = new Vue({
     el: "#app",
     data: {
         tipoTabla: 0,
-        perdidos: []
+        perdidos: [],
+        encontrados:[]
     },
     created() {
-        var url = 'http://localhost:8080/perdidos';
-        this.fetchData(url);
+        var urlPer = 'http://localhost:8080/perdidos';
+        var urlEnc = 'http://localhost:8080/encontrados';
+        this.fetchData(urlPer);
+        this.fetchDataEnc(urlEnc);
     },
     methods: {
-        fetchData(url) {
-            fetch(url)
+        fetchData(urlPer) {
+            fetch(urlPer)
                 .then(response => response.json())
                 .then(data => {
                     this.perdidos = data;
+                    console.log(data);
+                    this.loading = false;
+                    console.log(this.loading);
+                })
+                .catch(err => {
+                    this.errored = true
+                    console.error(err);
+                })
+        },
+        fetchDataEnc(urlEnc) {
+            fetch(urlEnc)
+                .then(response => response.json())
+                .then(data => {
+                    this.encontrados = data;
                     console.log(data);
                     this.loading = false;
                     console.log(this.loading);
