9bbcbd12e59039190f97c182ddfed9a552d13440
Javier Franco <ffranc@gmail.com>
2021-12-15 18:00:41 -0300

    cambiando url al de la base de datos heroku

--- src/main/resources/static/js/crear.js
+++ src/main/resources/static/js/crear.js
@@ -12,7 +12,7 @@ function grabarPerdido() {
         color: c,
     }
 
-    let url = "http://animalesperdidos.herokuapp.com/perdidos";
+    let url = "postgres://quwhxshzqyjtzy:fc6be089f887c0a86c5b3b1c8a1eab1017e1494eb6d74a35e8b3b94ab8d77761@ec2-23-23-181-251.compute-1.amazonaws.com:5432/perdidos";
     var options = {
         method: 'POST',
         body: JSON.stringify(perdidos),
@@ -41,7 +41,7 @@ function grabarEncontrado() {
             sexo: s
         }
 
-        let urlEnc = "http://animalesperdidos.herokuapp.com/encontrados";
+        let urlEnc = "postgres://quwhxshzqyjtzy:fc6be089f887c0a86c5b3b1c8a1eab1017e1494eb6d74a35e8b3b94ab8d77761@ec2-23-23-181-251.compute-1.amazonaws.com:5432/encontrados";
         var options = {
             method: 'POST',
             body: JSON.stringify(encontrados),
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,8 +6,8 @@ const app = new Vue({
         encontrados:[]
     },
     created() {
-        var urlPer = 'http://animalesperdidos.herokuapp.com/perdidos';
-        var urlEnc = 'http://animalesperdidos.herokuapp.com/encontrados';
+        var urlPer = 'postgres://quwhxshzqyjtzy:fc6be089f887c0a86c5b3b1c8a1eab1017e1494eb6d74a35e8b3b94ab8d77761@ec2-23-23-181-251.compute-1.amazonaws.com:5432/perdidos';
+        var urlEnc = 'postgres://quwhxshzqyjtzy:fc6be089f887c0a86c5b3b1c8a1eab1017e1494eb6d74a35e8b3b94ab8d77761@ec2-23-23-181-251.compute-1.amazonaws.com:5432/encontrados';
         this.fetchData(urlPer);
         this.fetchDataEnc(urlEnc);
     },
